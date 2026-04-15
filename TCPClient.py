f44cb99c813d3d1a0f566daf86bcfedd56ff7f32
liao.749 <liao.749@osu.edu>
2026-04-15 14:21:47 -0400

    TCP socket and connection setup

--- TCPClient.py
+++ TCPClient.py
@@ -1,7 +1,9 @@
-import sys
-import os
-import random
-import string
 from socket import *
 
-# create TCP client socket
+# servername = server IP
+serverName = ""
+serverPort = 45231
+
+# create TCP socket and connect to server
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
